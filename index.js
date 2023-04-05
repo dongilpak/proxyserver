628f9a937ea5b9313cac18113e362c64ba1d9807
dongilpak <ehddlf2395@gmail.com>
2023-04-05 18:20:35 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+const express = require('express');
+
+app.use(cors());
+
+const app = express();
+
+const port = 8000;
+
+app.get('/result', async (req, res) => {
+    const resp = await fetch(
+        'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1061'
+    );
+
+    const data = await resp.json();
+    res.send(data);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
